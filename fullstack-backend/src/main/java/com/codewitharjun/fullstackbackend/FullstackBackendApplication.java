22b1a99b55c3ca7d98276dd1c7d5869e81e08d86
amazonclonepage <prashanthdharavath449@gmail.com>
2025-10-15 22:55:13 +0530

    first commit

---
+++ fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/FullstackBackendApplication.java
@@ -0,0 +1,15 @@
+package com.codewitharjun.fullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackendApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello Spring");
+
+        SpringApplication.run(FullstackBackendApplication.class, args);
+	}
+
+}
